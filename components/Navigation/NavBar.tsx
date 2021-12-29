375e6aea54a0716edbb6b40480d7f5d1f19e6d03
rubengarciabri <ruben@behaviourlab.com>
2021-12-29 20:19:03 +0100

    feat(initial-migration): Create basic nav component

--- components/Navigation/NavBar.tsx
+++ components/Navigation/NavBar.tsx
@@ -1,7 +1,39 @@
 import * as React from "react"
 
-const NavBar = () => {
-  return <div></div>
+import Link from "next/link"
+import { useRouter } from "next/router"
+import CardBase from "../Cards/CardBase"
+
+interface IPropTypes {
+  // TODO: Make more specific
+  linksArray: Array<any>
+}
+
+const NavBar = ({ linksArray }: IPropTypes) => {
+  const router = useRouter()
+  return (
+    <CardBase>
+      <ul className="">
+        {linksArray &&
+          linksArray.map(({ title, url, Icon }) => {
+            return (
+              <li className="">
+                <Link href={url}>
+                  {/* Make active link text bold */}
+                  <a
+                    className={`block p-4 border-b border-gray-300 ${
+                      router.pathname === url && "font-bold"
+                    } `}
+                  >
+                    {title}
+                  </a>
+                </Link>
+              </li>
+            )
+          })}
+      </ul>
+    </CardBase>
+  )
 }
 
 export default NavBar
--- components/Root/Layout.tsx
+++ components/Root/Layout.tsx
@@ -8,15 +8,53 @@ interface IPropTypes {
   children: React.ReactNode
 }
 
+const linksArray = [
+  {
+    title: "Home",
+    url: "/",
+    Icon: () => {
+      return
+    },
+  },
+  {
+    title: "Friends",
+    url: "/friends",
+    Icon: () => {
+      return
+    },
+  },
+  {
+    title: "Photos",
+    url: "/photos",
+    Icon: () => {
+      return
+    },
+  },
+  {
+    title: "Chat",
+    url: "/chat",
+    Icon: () => {
+      return
+    },
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    Icon: () => {
+      return
+    },
+  },
+]
+
 const Layout = ({ children }: IPropTypes) => {
   return (
     <>
       <TopBar />
       <div className="main-container">
         <div className="flex justify-between mt-8 alig-center gap-x-16">
-          <div className="basis-1/4">
+          <div className="flex flex-col gap-8 basis-1/4">
             <ProfileCard name="Ruben Garcia" userName="@rubenGB" />
-            <NavBar />
+            <NavBar linksArray={linksArray} />
           </div>
           <div className="basis-2/4">{children}</div>
           <div className="basis-1/4"></div>
