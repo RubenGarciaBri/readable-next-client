d1f9df7165665890c3dbdfe43650545178336953
rubengarciabri <ruben@behaviourlab.com>
2021-12-30 23:56:57 +0100

    feat(newpost-modal): Created Modal Component for creating new posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,18 @@ import * as React from "react"
 import type { NextPage } from "next"
 import Layout from "../components/Root/Layout"
 import CreatePost from "../components/CreatePost/CreatePost"
+import CreatePostModal from "../components/CreatePost/Modal/CreatePostModal"
 
 const Home: NextPage = () => {
+  const [showModal, setShowModal] = React.useState(false)
+
   return (
     <div>
+      {/* <button onClick={() => setShowModal(true)}>Open Modal</button> */}
       <Layout>
         <CreatePost />
       </Layout>
+      <CreatePostModal onClose={() => setShowModal(false)} show={showModal} />
     </div>
   )
 }
